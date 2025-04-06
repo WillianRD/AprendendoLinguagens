532fe4295213ded70b8af63b931bf045df4654b7
WillianRD <willianrodriguesdos1@gmail.com>
2025-04-06 14:37:44 -0300

    JavaScript - Estudos sobre API

--- API/script.js
+++ API/script.js
@@ -1,10 +1,9 @@
 function buscarGato() {
-    fetch("https://api.thecatapi.com/v1/images/search")
-      .then(response => response.json())
-      .then(data => {
-        const urlImagem = data[0].url;
+    fetch("https://api.thecatapi.com/v1/images/search") // Url da API
+      .then(response => response.json()) // Converte os dados em formato JSON
+      .then(data => { // Json retorna os dados
+        const urlImagem = data[0].url; // .url e o campo da API
         document.getElementById("imagemGato").src = urlImagem;
-        window.onload(buscarGato)
       })
       .catch(error => {
         console.error("Erro ao buscar gato:", error);
